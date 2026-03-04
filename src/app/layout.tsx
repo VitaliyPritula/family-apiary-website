baa5f4702a1a9973e00c40919b636f6cd2043bb4
Vitaly Pritula <54078944+VitaliyPritula@users.noreply.github.com>
2026-03-04 12:59:24 +0200

    add import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Header from "./components/layout/header/Header";
 import Footer from "./components/layout/footer/Footer";
-import Cartdrawer from "./components/contants/CartDrawer";
+import Cartdrawer from "../app/components/contants/CartDrawer";
+
 import "./globals.css";
 
 const geistSans = Geist({
