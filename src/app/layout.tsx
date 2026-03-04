23adee903ae2913ba094745b6247be84f954b947
Vitaly Pritula <54078944+VitaliyPritula@users.noreply.github.com>
2026-03-04 13:14:17 +0200

    add build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import Cartdrawer from "@/app/components/contants/CartDrawer";
+import Cartdrawer from "@/app/components/CartDrawer";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Footer from "./components/layout/footer/Footer";
